7a4a5e1998e60ce9d1b29424a24594854d9ad4f4
Aidvi <nissen.magnus@outlook.dk>
2018-12-11 14:42:35 +0100

    Fixed database friend list

--- migrations/20181031102513_name.js
+++ migrations/20181031102513_name.js
@@ -6,6 +6,13 @@ exports.up = function(knex, Promise) {
             table.string('username');
             table.string('password');
         })
+        .createTable("friend_list", function(table){
+            table.increments("id").primary();
+            table.integer("user_id").unsigned().notNullable();
+            table.integer("friend_id").unsigned().notNullable();
+            table.foreign("user_id").references("users.id")
+            table.foreign("friend_id").references("users.id")
+        })
         .createTable('rooms', function(table) {
             table.increments('id').primary();
             table.string('name');
@@ -32,5 +39,6 @@ exports.down = function(knex, Promise) {
   	.dropTableIfExists('users')
   	.dropTableIfExists('messages')
   	.dropTableIfExists('rooms')
-  	.dropTableIfExists('user_rooms');
+    .dropTableIfExists('user_rooms')
+    .dropTableIfExists("friend_list");
 };
