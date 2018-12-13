de6df77850d178394a3274e91a4609289e705cb8
Aidvi <nissen.magnus@outlook.dk>
2018-12-13 10:30:49 +0100

    Commit

--- routes/room.js
+++ routes/room.js
@@ -18,13 +18,15 @@ app.post('/create-room', (req, res) => {
 
     if (name && req.session.isLoggedIn === true) {
 
-        db.Room.query().select().where({ name }).then(userArray => {
-            if (userArray.length > 0) {
+        db.Room.query().select().where({ name: name }).then(roomArray => {
+            if (roomArray.length > 0) {
                 console.log("room name already exists")
+                console.log("TEST ROOM ALREADY EXIST");
+                
                 res.send({ "status": 200, "response": "room name already exist" });
             } else {
-                db.Room.query().insert({ name }).then(persistedData => {
-                    console.log("everything went well");
+                db.Room.query().insert({ name: name }).then(persistedData => {
+                    console.log("everything went well" + "ROOM CREATED");
                     console.log(persistedData);
                     res.send({ "status": 200, "response": "everything went well" });
                 })
@@ -33,16 +35,8 @@ app.post('/create-room', (req, res) => {
         })
     }
     else {
-        //res.send("Enter a room name!");
-        if (req.session.isLoggedIn == true) {
-            var path = require('path')
-            res.sendFile(path.resolve(__dirname + '/../public/chatroom.html'));
-            console.log("Denne bruger inde på ", req.session.username);
-        }
-        else {
             var path = require('path')
             res.sendFile(path.resolve(__dirname + '/../public/login.html'));
         }
-    }
     })
  }
